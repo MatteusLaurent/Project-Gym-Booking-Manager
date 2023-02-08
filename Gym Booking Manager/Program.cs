926a3c200fdff45c551b3965f2d70c6b8c0b973e
Henrik Johansson <henrik@soltek.se>
2023-02-08 15:55:48 +0100

    added reading of data to user class

--- Gym Booking Manager/Program.cs
+++ Gym Booking Manager/Program.cs
@@ -8,14 +8,7 @@ namespace Gym_Booking_Manager
         static void Main(string[] args)
         {
             List <User> users = new List <User> ();
-            string[] lines = File.ReadAllLines("Users.txt");
-            foreach (string line in lines)
-            {
-                string[] strings = line.Split(";");
-                if (strings[0]=="Staff")users.Add (new Staff(int.Parse(strings[1]), strings[2], int.Parse(strings[3]), strings[4], strings[5], strings[6], strings[7]));
-                if (strings[0] == "Admin") users.Add(new Admin(int.Parse(strings[1]), strings[2], int.Parse(strings[3]), strings[4], strings[5], strings[6], strings[7]));
-                if (strings[0] == "Customer") users.Add(new Customer(int.Parse(strings[1]), strings[2], int.Parse(strings[3]), strings[4], strings[5], strings[6], strings[7], DateTime.Parse(strings[8]), DateTime.Parse(strings[9]), bool.Parse(strings[10])));
-            }
+            User.AddUsers(users);                
         }        
         // Static methods for the program
     }
